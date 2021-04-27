0c446a48e7458c080587a8b12a9a46a7c7f70dcb
Logan Thomas <logannormanthomas@protonmail.com>
2021-04-27 20:33:00 +0000

    refactured console logs

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,22 +1,22 @@
-const eqArrays = (actual, expected) => {
+const assertArraysEqual = (actual, expected) => {
   if (actual.length !== expected.length) {
-    console.log('😡😡😡The Arrays do not match!');
+    console.log('😡😡😡 The Arrays do not match!');
     return false;
   } else {
     for (let i = 0; i < actual.length; i++) {
       if (actual[i] !== expected[i]) {
-        console.log('😡😡😡The Arrays do not match!');
+        console.log('😡😡😡 The Arrays do not match!');
         return false;
       }
     }
   }
-  console.log('😀😀😀The Arrays do match!');
+  console.log('😀😀😀 The Arrays do match!');
   return true;
 };
 
 
-eqArrays([1, 2, 3], [1, 2, 3]); // => true
-eqArrays([1, 2, 3], [3, 2, 1]); // => false
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
+assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
 
-eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => false
