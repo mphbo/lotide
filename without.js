3e78a84fa60e492d6dea80de03927a04b62a8671
Logan Thomas <logannormanthomas@protonmail.com>
2021-04-27 20:51:12 +0000

    added new without.js to my lotide project

---
+++ without.js
@@ -0,0 +1,34 @@
+const assertArraysEqual = (actual, expected) => {
+  if (actual.length !== expected.length) {
+    console.log('😡😡😡 The Arrays do not match!');
+    return false;
+  } else {
+    for (let i = 0; i < actual.length; i++) {
+      if (actual[i] !== expected[i]) {
+        console.log('😡😡😡 The Arrays do not match!');
+        return false;
+      }
+    }
+  }
+  console.log('😀😀😀 The Arrays do match!');
+  return true;
+};
+
+const without = (source, itemsToRemove) => {
+  let goodCandy = source;
+  for (let i = 0; i < source.length; i++) {
+    for (let j = 0; j < itemsToRemove.length; j++) {
+      if (source[i] === itemsToRemove[j]) {
+        goodCandy.splice(i, 1);
+      }
+    }
+  }
+  // console.log(goodCandy);
+  return goodCandy;
+};
+
+without([1, 2, 3], [1]); // => [2, 3]
+without(["1", "2", "3"], [1, 2, "3"]); // => ["1", "2"]
+
+assertArraysEqual(without([1, 2, 3], [1]), [2, 3]);
+assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]);
