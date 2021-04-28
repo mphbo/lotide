017ec34535084d9489cd83ef99b5cc2418b76cae
Logan Thomas <logannormanthomas@protonmail.com>
2021-04-28 03:35:51 +0000

    finished letterPositions.js

---
+++ letterPositions.js
@@ -0,0 +1,35 @@
+const countLetters = sentence => {
+  let positionOfLetters = {};
+  sentence = sentence.split(' ').join('');
+
+  for (let i = 0; i < sentence.length; i++) {
+    if (positionOfLetters[sentence[i]]) {
+      positionOfLetters[sentence[i]].push(i);
+    } else {
+      positionOfLetters[sentence[i]] = [];
+      positionOfLetters[sentence[i]].push(i); 
+    }
+  }
+  return positionOfLetters;
+};
+
+console.log(countLetters('lighthouse in the house'));
+
+
+const assertArraysEqual = (actual, expected) => {
+  if (actual.length !== expected.length) {
+    console.log('😡😡😡 The Arrays do not match!');
+    return false;
+  } else {
+    for (let i = 0; i < actual.length; i++) {
+      if (actual[i] !== expected[i]) {
+        console.log('😡😡😡 The Arrays do not match!');
+        return false;
+      }
+    }
+  }
+  console.log('😀😀😀 The Arrays do match!');
+  return true;
+};
+
+assertArraysEqual(countLetters('hello'), {h: [0], e: [1], l: [2, 3], o: [4]})
