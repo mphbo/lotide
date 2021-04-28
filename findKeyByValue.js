c2f700a6bc8550f8a3d149d35e73c37f4f7eb1d5
Logan Thomas <logannormanthomas@protonmail.com>
2021-04-28 03:53:39 +0000

    completed findKeyByValue.js

---
+++ findKeyByValue.js
@@ -0,0 +1,32 @@
+const findKeyByValue = (object, value) => {
+  let theKey = '';
+  for (let key in object) {
+    if (object[key] === value) {
+      theKey = key;
+    }
+  }
+  if (theKey === '') {
+    return undefined;
+  } else {
+    return theKey;
+  }
+};
+
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😀😀😀  Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😡😡😡  Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
