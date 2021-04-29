cf6c7f0a14fcd3050e04ded3b63aa34cd0640461
Logan Thomas <logannormanthomas@protonmail.com>
2021-04-29 03:26:16 +0000

    completed map.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -19,4 +19,4 @@ assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
 assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
 
 assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => false
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => falseO
---
+++ map.js
@@ -0,0 +1,39 @@
+const map = (array, func) => {
+  const results = [];
+  for (let item of array) {
+    results.push(func(item));
+  }
+  return results;
+}
+
+let words = ["ground", "control", "to", "major", "tom"];
+
+
+let results1 = map(words, word => word[0]);
+console.log(results1);
+
+
+const assertArraysEqual = (actual, expected) => {
+  if (actual.length !== expected.length) {
+    console.log('😡😡😡 The Arrays do not match!');
+    return false;
+  } else {
+    for (let i = 0; i < actual.length; i++) {
+      if (actual[i] !== expected[i]) {
+        console.log('😡😡😡 The Arrays do not match!');
+        return false;
+      }
+    }
+  }
+  console.log('😀😀😀 The Arrays do match!');
+  return true;
+};
+
+assertArraysEqual(results1, ['g', 'c', 't', 'm', 't']);
+results1 = map(words, word => word[1]);
+assertArraysEqual(results1, ['r', 'o', 'o', 'a', 'o']);
+
+words = ['Hi', 'I', 'love', 'coding']
+results1 = map(words, word => word[0]);
+
+assertArraysEqual(results1, ['H', 'I', 'l', 'c']);
