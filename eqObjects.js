0d9614d1654705a589c0c085ae4ab39bf0f094a4
Logan Thomas <logannormanthomas@protonmail.com>
2021-05-01 01:01:52 +0000

    completed eqObjects with recursion

--- eqObjects.js
+++ eqObjects.js
@@ -26,25 +26,36 @@ const eqArrays = (actual, expected) => {
 
 const eqObjects = (object1, object2) => {
   if (Object.keys(object1).length !== Object.keys(object2).length) {
-    console.log(false);
     return false;
   }
   for (let key in object1) {
     if (Array.isArray(object1[key])) {
       return eqArrays(object1[key], object2[key])
     } else if (object2[key] === undefined) {
-      console.log(false);
       return false;
-    } else if (object1[key] !== object2[key]) {
-      console.log(false);
+    } else if (typeof object1[key] === 'object') {
+      return eqObjects(object1[key], object2[key]);
+    }else if (object1[key] !== object2[key]) {
       return false;
     }
   }
-  console.log(true);
   return true;
+
 }
+console.log(eqObjects(
+  {
+   a: { z: 1 }, b: 2 
+  }, 
+  { 
+    a: { z: 1 }, b: 2 
+  })); // => true
 
-
+console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 })); // => false
+console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 })); // => false
+console.log(eqObjects(
+    { a: { y: 0}, l: { t: { b:3, k: 5}, z: 1} }, 
+    { a: { y: 0}, l: { t: { b:3, k: 5}, z: 1} }
+  ));
 
 // const ab = { a: "1", b: "2" };
 // const ba = { b: "2", a: "1" };
@@ -53,12 +64,12 @@ const eqObjects = (object1, object2) => {
 // const abc = { a: "1", b: "2", c: "3" };
 // eqObjects(ab, abc); // => false
 
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-eqObjects(cd, dc); // => true
+// const cd = { c: "1", d: ["2", 3] };
+// const dc = { d: ["2", 3], c: "1" };
+// eqObjects(cd, dc); // => true
 
-const cd2 = { c: "1", d: ["2", 3, 4] };
-eqObjects(cd, cd2); // => false
+// const cd2 = { c: "1", d: ["2", 3, 4] };
+// eqObjects(cd, cd2); // => false
 
 
 // assertEqual(ab, ba);
