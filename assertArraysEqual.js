4493d5c3834ebd3bd07ed2ca9673d8579007d52f
Logan Thomas <logannormanthomas@protonmail.com>
2021-05-03 20:27:18 +0000

    simplified assertArrayEqual with imports and exports

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,19 +1,14 @@
+const eqArrays = require("./eqArrays");
+
 const assertArraysEqual = (actual, expected) => {
-  if (actual.length !== expected.length) {
-    console.log('😡😡😡 The Arrays do not match!');
-    return false;
+  if (eqArrays(expected, actual)) {
+    return '😀😀😀 The Arrays do match!';
   } else {
-    for (let i = 0; i < actual.length; i++) {
-      if (actual[i] !== expected[i]) {
-        console.log('😡😡😡 The Arrays do not match!');
-        return false;
-      }
-    }
+    return '😡😡😡 The Arrays do not match!'
   }
-  console.log('😀😀😀 The Arrays do match!');
-  return true;
 };
 
+module.exports = assertArraysEqual;
 
 assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
 assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
